477a5b43db7dba8da57ed9389d14cf57fdb7cd17
dengxuan <you@example.com>
2022-05-10 12:15:42 +0800

    增加内存采集

--- src/perfcat/pages/profiler/plugins/__init__.py
+++ src/perfcat/pages/profiler/plugins/__init__.py
@@ -1,4 +1,5 @@
 from .base import MonitorChart
 from .cpu_monitor import CpuMonitor
+from .mem_monitor import MemMonitor
 
-register: list[MonitorChart] = [CpuMonitor]
+register: list[MonitorChart] = [CpuMonitor, MemMonitor]
--- src/perfcat/pages/profiler/plugins/mem_monitor.py
+++ src/perfcat/pages/profiler/plugins/mem_monitor.py
@@ -1,18 +1,40 @@
 from ppadb.device import Device
 from .base.chart import MonitorChart
-from ..utils.topparser import top_to_dict
-from .cpu_normalize import normalize
 
 
 class MemMonitor(MonitorChart):
     def __init__(self, parent=None):
         super().__init__(
-            series_names=["Total Mem", "App Mem"],
-            formatter={},
+            series_names=[
+                "pss",
+                "private dirty",
+                "private clean",
+                "swapped dirty",
+                "heap size",
+                "heap alloc",
+                "heap free",
+            ],
+            formatter={
+                "pss": lambda v: f"{v}MB",
+                "private dirty": lambda v: f"{v}MB",
+                "private clean": lambda v: f"{v}MB",
+                "swapped dirty": lambda v: f"{v}MB",
+                "heap size": lambda v: f"{v}MB",
+                "heap alloc": lambda v: f"{v}MB",
+                "heap free": lambda v: f"{v}MB",
+            },
             y_axis_name="MEM",
             parent=parent,
         )
-        self.pid = None
 
     def tick(self, sec: int, device: Device, package_name: str):
-        pass
+
+        mem_info = device.get_meminfo(package_name)
+
+        self.add_point("pss", sec, mem_info.pss / 1024)
+        self.add_point("private dirty", sec, mem_info.private_dirty / 1024)
+        self.add_point("private clean", sec, mem_info.private_clean / 1024)
+        self.add_point("swapped dirty", sec, mem_info.swapped_dirty / 1024)
+        self.add_point("heap size", sec, mem_info.heap_size / 1024)
+        self.add_point("heap alloc", sec, mem_info.heap_alloc / 1024)
+        self.add_point("heap free", sec, mem_info.heap_free / 1024)
