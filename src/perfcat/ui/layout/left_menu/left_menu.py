a62ecc21cf2eaec95d78b01d231d327d408b4dba
dengxuan <you@example.com>
2022-05-11 19:35:44 +0800

    增加菜单展开关闭状态还原

--- src/perfcat/ui/layout/left_menu/left_menu.py
+++ src/perfcat/ui/layout/left_menu/left_menu.py
@@ -14,6 +14,8 @@
 
 
 import textwrap
+import PySide6
+import logging
 from PySide6.QtWidgets import (
     QWidget,
     QGraphicsDropShadowEffect,
@@ -21,10 +23,12 @@ from PySide6.QtWidgets import (
     QVBoxLayout,
     QPushButton,
 )
-from PySide6.QtCore import Slot, QPropertyAnimation, QEasingCurve
-from PySide6.QtGui import QIcon, QPixmap
+from PySide6.QtCore import QTimer
+from perfcat.settings import settings
 from .ui_left_menu import Ui_LeftMenu
 from .. import util
+
+log = logging.getLogger(__name__)
 
 
 class LeftMenu(QWidget, Ui_LeftMenu):
@@ -52,6 +56,13 @@ class LeftMenu(QWidget, Ui_LeftMenu):
         self.bottom_btn_group.addButton(self.btn_setting, 0)
         self.bottom_btn_group.addButton(self.btn_about, 1)
 
+    def showEvent(self, event: PySide6.QtGui.QShowEvent) -> None:
+        # 读取上一次展开关闭状态
+        expanded = settings.value("left_menu/expanded", True, bool)
+        self.expand(expanded)
+        log.debug(f"读取上一次展开关闭状态 {expanded}")
+        return super().showEvent(event)
+
     def add_nav_menu(self, icon, text, binding_page_name):
         indented_text = textwrap.indent(text, " " * 4)  # 缩进4格用来适配按钮样式
         btn_menu = QPushButton(icon, indented_text, self)
@@ -65,12 +76,13 @@ class LeftMenu(QWidget, Ui_LeftMenu):
             btn_menu.setChecked(True)
 
     def bottom_btn_group_reset(self):
-        self.bottom_btn_group.setExclusive(False)
         for btn in self.bottom_btn_group.buttons():
             btn.setChecked(False)
         self.bottom_btn_group.setExclusive(True)
 
     def expand(self, checked):
+        log.debug(f"展开关闭导航菜单 {checked}")
+        settings.setValue("left_menu/expanded", checked)
         util.set_h_expand_anim(
             self.parentWidget(), checked, self.MAX_WIDTH, self.MIN_WIDTH
         )
