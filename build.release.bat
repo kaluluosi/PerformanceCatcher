cfd133a33d8c17286386bb2b437dce5e12dea11a
dengxuan <you@example.com>
2022-05-13 14:12:43 +0800

    区分开发和发布build脚本 发布时去掉控制台显示

---
+++ build.release.bat
@@ -0,0 +1,13 @@
+nuitka ^
+--standalone ^
+--follow-imports ^
+--show-progress ^
+--plugin-enable=pyside6 ^
+--include-data-dir=src/perfcat/assets=perfcat/assets  ^
+--output-dir=dist\win ^
+--windows-disable-console ^
+--windows-icon-from-ico=src/perfcat/assets/logo.ico ^
+--windows-company-name="xinyuanstudio" ^
+--windows-file-version="1.0.0" ^
+--windows-file-description="性能测试工具" ^
+.\src\perfcat\PerfCat.py
