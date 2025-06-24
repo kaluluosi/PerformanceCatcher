f04ad62b38784b4a6885da7a65ece42eeb51a657
kaluluosi111 <kaluluosi@gmail.com>
2025-06-24 16:09:54 +0800

    打包测试

---
+++ prod.py
@@ -0,0 +1,14 @@
+import perfcat
+import secrets
+from nicegui import ui, app
+
+app.native.window_args["resizable"] = True
+app.native.start_args["debug"] = False
+
+ui.run(
+    native=True,
+    reload=False,
+    window_size=(1280, 768),
+    title="PerformanceCatcher",
+    storage_secret=secrets.token_hex(16),
+)
--- src/perfcat/__main__.py
+++ src/perfcat/__main__.py
@@ -1,5 +1,7 @@
 from nicegui import ui,app
-app.native.window_args['resizable'] = True
-app.native.start_args['debug'] = True
 
-ui.run(native=True,reload=False,window_size=(1024,768),title="PerformanceCatcher",frameless=False)
+if __name__ == "__main__":
+    app.native.window_args['resizable'] = True
+    app.native.start_args['debug'] = True
+
+    ui.run(native=True,reload=False,window_size=(1024,768),title="PerformanceCatcher",frameless=False)
