6f04666d98d068253e60f816ac385935394f771d
kaluluosi111 <kaluluosi@gmail.com>
2025-07-04 15:18:30 +0800

    关闭程序的时候kill掉adb服务

--- src/perfcat/__main__.py
+++ src/perfcat/__main__.py
@@ -2,8 +2,8 @@ from nicegui import ui, app
 
 if __name__ == "__main__":
     app.native.window_args["resizable"] = True
-    app.native.start_args["debug"] = True
     app.native.window_args["text_select"] = True
+    app.native.start_args["debug"] = True
 
     ui.run(
         native=True,
